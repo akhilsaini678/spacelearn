8918734e25630155441b88468ec6ffb4ba150ce4
Akhil Saini <akhilsaini678@gmail.com>
2022-01-03 16:26:36 +0530

    Contact Message Feature Added

--- aakashganga/settings.py
+++ aakashganga/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4qa8b0umpfncz9lbvueb*@d4)uo($oa&*-u-(iv9zyw5p3vrzx'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -130,7 +130,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'akhilsainiwork@gmail.com'
-EMAIL_HOST_PASSWORD = 'Akki8171@'
+EMAIL_HOST_PASSWORD = os.environ['EMAIL_PASSWORD']
 EMAIL_PORT = '587'
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
+
+
+CSRF_TRUSTED_ORIGINS = ['aakashganga.herokuapp.com']
